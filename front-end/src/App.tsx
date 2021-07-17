e6114013f66d27ff0faa03d677d9ece9e055dab4
Tareq Sabra <Tareqsabra949@gmail.com>
2021-07-18 00:26:02 +0300

    0.7

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./componants/NavBar/nav";
-
+<script src="https://unpkg.com/react/umd/react.production.min.js"></script>;
+<script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>;
 function App() {
+  const user = {
+    Name: "Tareq Sabra",
+    ID: 11611282,
+    Pass: "1124as59",
+  };
   return (
     <div className="App">
-      <NavBar />
+      <NavBar user={user} />
     </div>
   );
 }
--- front-end/src/componants/NavBar/nav.tsx
+++ front-end/src/componants/NavBar/nav.tsx
@@ -1,9 +1,11 @@
-import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import "../NavBar/NavStyle.scss";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 import logo from "../../Resoruces/Pics/logo/logo.png";
 import Container from "react-bootstrap/Container";
+import "../NavBar/NavStyle.css";
+import React, { useState, useEffect } from "react";
 interface Props {
   user?: {
     Name: string;
@@ -12,23 +14,58 @@ interface Props {
   };
 }
 export const NavBar: React.FC<Props> = ({ user }) => {
+  const [hidden, setlhidden] = useState(true);
+  const [Notice, setlNotice] = useState("you must be logged in");
+  const [dispabled, setldispabled] = useState(true);
+  useEffect(() => {
+    if (user === undefined) {
+      setlhidden(true);
+      setldispabled(true);
+      setlNotice("you must be logged in");
+    } else {
+      setlhidden(false);
+      setldispabled(false);
+      setlNotice(`View your Fan Club ${user.Name}`);
+    }
+  }, [user]);
+  const renderTooltip = (props: any) => (
+    <Tooltip id="button-tooltip" {...props}>
+      {Notice}
+    </Tooltip>
+  );
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img alt="logo" src={logo} className="logoIcon" />
-          </Navbar.Brand>
-          <Navbar.Brand href="#home" className="logoTitel">
-            Mena
-          </Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img alt="logo" src={logo} className="logoIcon" />
+        </Navbar.Brand>
+        <Navbar.Brand href="#home" className="logoTitel">
+          Mena
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto m-5">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Gallery</Nav.Link>
-            <Nav.Link href="#pricing">Fan Club</Nav.Link>
+            <OverlayTrigger
+              placement="right"
+              delay={{ show: 250, hide: 400 }}
+              overlay={renderTooltip}
+            >
+              <Container>
+                <Nav.Link disabled={dispabled} href="#pricing">
+                  Fan Club
+                </Nav.Link>
+              </Container>
+            </OverlayTrigger>
           </Nav>
-        </Container>
-      </Navbar>
-    </div>
+          <Nav>
+            <Nav.Link hidden={hidden}>
+              <Navbar.Text className="text-light">{`Welcome : ${user?.Name}!`}</Navbar.Text>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
